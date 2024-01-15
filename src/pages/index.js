e8b19d5663deda6e229aa4abfac524d35baee414
BunnY <w@bunnywong.com>
2024-01-16 07:20:18 +0800

    clean up as blank page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,7 @@
-import React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <StaticImage
-        src="../images/gatsby-astronaut.png"
-        alt="gatsby astronaut"
-      />
-    </div>
-    <p><Link to="/page-2/">Go to page 2</Link></p>
-    <p><Link to="/using-typescript/">Go to "Using TypeScript"</Link></p>
-  </Layout>
-)
+const IndexPage = () => {
+  return <main></main>
+}
 
 export default IndexPage
