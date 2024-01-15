918ba135590729d5aabfdba448d2bc49df7a6ff3
BunnY <w@bunnywong.com>
2024-01-16 07:55:08 +0800

    add inline filter feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -176,6 +176,24 @@ const IndexPage = () => {
       </p>
     )
   }
+  const Filter = ({ item }) => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5 text-gray-400 inline cursor-pointer"
+        onClick={() =>
+          setCaregiverName(`${item.lastName} ${item.firstName} ${item.chiName}`)
+        }
+      >
+        <path
+          fillRule="evenodd"
+          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+        />
+      </svg>
+    )
+  }
   const Card = ({ item }) => (
     <div key={item.userId} className="p-3 grid grid-cols-8 gap-4 border-b-2">
       {/* card LHS */}
@@ -193,9 +211,10 @@ const IndexPage = () => {
       {/* card RHS */}
       <div className="col-span-7">
         <Duration start={item.startedAt} end={item.endedAt} />
-        <p>
+        <span>
           {item.userId} - {item.lastName} {item.firstName} {item.chiName}
-        </p>
+          <Filter item={item} />
+        </span>
         <p>
           <strong
             className="text-xl"
