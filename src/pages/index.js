c428ced79055bc91c6ed03b55cf4ab827f4e22ed
BunnY <w@bunnywong.com>
2024-01-16 07:27:28 +0800

    add search feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,24 @@ const IndexPage = () => {
   const [schedule, setSchedule] = useState([])
   const [selectedItems, setSelectedItems] = useState([])
   const [selectedMonths, setSelectedMonths] = useState([])
+  const [caregiverName, setCaregiverName] = useState(null)
 
   // utils:
+  // dynamic update caregiver data
+  const getCaregiver = () => {
+    if (!caregiverName) {
+      return _dataCaregiver
+    }
+    return _.filter(_dataCaregiver, (user) => {
+      const caregiverNameLabel = `${user.lastName} ${user.firstName}${user.chiName}`
+      const caregiverNameLabelWithGap = `${user.lastName} ${user.firstName} ${user.chiName}`
+      const _caregiverName = caregiverName.toLocaleLowerCase()
+      return (
+        caregiverNameLabel.toLocaleLowerCase().match(_caregiverName) ||
+        caregiverNameLabelWithGap.toLocaleLowerCase().match(_caregiverName)
+      )
+    })
+  }
   // bulk process with `monthString`, otherwise single's `item`
   const updateStatus = ({ monthString, item, status }) => {
     const _schedule = _.flattenDeep(
@@ -231,14 +247,48 @@ const IndexPage = () => {
       </>
     )
   }
+  const Search = () => {
+    return (
+      <div className="mb-3 p-3 sticky top-0 bg-white">
+        <strong className="float-left">Caregiver Name</strong>
+        <div className="float-left relative w-64">
+          <input
+            autoFocus
+            type="search"
+            className="p-1 px-2 ml-3 h-8 border relative w-full"
+            placeholder="Search"
+            value={caregiverName}
+            onChange={(e) => setCaregiverName(e.target.value)}
+          />
+          <div
+            className="absolute top-2 right-0 p-1"
+            style={{ top: '1px', right: '-10px' }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5 text-gray-400"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   useEffect(() => {
-    const _schedule = getScheduleDaily(_dataCaregiver)
+    const _schedule = getScheduleDaily(getCaregiver())
     setSchedule(_schedule)
   }, [caregiverName]) // eslint-disable-line
 
   return (
     <main>
+      <Search />
       <div className="container mx-auto flex">
         {Object.keys(schedule).map((month, index) => (
           <Column monthString={month} days={schedule[month]} key={index} />
